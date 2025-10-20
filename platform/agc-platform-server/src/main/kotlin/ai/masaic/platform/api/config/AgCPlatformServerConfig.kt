de553d8a6dbf5a27f57831dab3cdd33786d59d1a
JB <jasbirsinghkamboj@gmail.com>
2025-10-20 10:45:11 +0530

    - Fixed Platform Info config loading.

--- platform/agc-platform-server/src/main/kotlin/ai/masaic/platform/api/config/AgCPlatformServerConfig.kt
+++ platform/agc-platform-server/src/main/kotlin/ai/masaic/platform/api/config/AgCPlatformServerConfig.kt
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import java.net.URI
 
-// @Configuration
+@Configuration
 class AgCPlatformServerConfig {
     @Bean
     @ConditionalOnMissingBean
@@ -29,11 +29,17 @@ class AgCPlatformServerConfig {
         partners: Partners,
         @Value("\${platform.deployment.oauth.redirectAgcHost:na}") agcPlatformRedirectBaseUrl: String = "na",
         @Value("\${platform.deployment.oauth.agcUiHost:na}") agcUiHost: String = "na",
+        @Value("\${platform.deployment.agc-cs-runtime.path:../agc-client-runtime/java-sdk}") agcRuntimePath: String,
+        @Value("\${platform.deployment.agc-cs-runtime.securitykey:na}") securityKey: String,
+        @Value("\${platform.deployment.multiplug.enabled:false}") multiPlugEnabled: Boolean,
     ): PlatformInfo {
         val vectorStoreInfo =
             if (vectorSearchProviderType == "qdrant") VectorStoreInfo(true) else VectorStoreInfo(false)
 
         val oAuthRedirectSpecs = if (agcPlatformRedirectBaseUrl != "na" && agcUiHost != "na") OAuthRedirectSpecs(URI(agcPlatformRedirectBaseUrl), URI(agcUiHost)) else OAuthRedirectSpecs()
+
+        if (multiPlugEnabled && securityKey == "na") throw IllegalStateException("property platform.deployment.agc-cs-runtime.securityKey is not defined")
+
         return PlatformInfo(
             version = "v${buildProperties.version}",
             buildTime = buildProperties.time,
@@ -51,7 +57,7 @@ class AgCPlatformServerConfig {
                 },
             partners = partners,
             oAuthRedirectSpecs = oAuthRedirectSpecs,
-            agentClientSideRuntimeConfig = AgentClientSideRuntimeConfig("", ""),
+            agentClientSideRuntimeConfig = AgentClientSideRuntimeConfig(agcRuntimePath, securityKey),
         )
     }
 
